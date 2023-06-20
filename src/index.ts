88c9ae9ee741510506dee62213a4f08772d1769b
stoXmod <nisharasenadheera2@gmail.com>
2023-06-20 08:15:11 +0530

    feat: export all default exports from Langchain

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import defaultExport from "langchain";
+
 export * from "./services/PineconeService";
 export * from "./services/ChainService";
 export * from "./services/PromptBaseService";
 export * from "./services/SummarizationService";
 export * from "langchain";
+export default defaultExport;
