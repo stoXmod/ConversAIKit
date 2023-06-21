817c5018e0f72966822462e5b280c015c666f306
stoXmod <nisharasenadheera2@gmail.com>
2023-06-21 13:53:25 +0530

    feat: Add advance options to Chain Service

--- src/services/ChainService.ts
+++ src/services/ChainService.ts
@@ -2,16 +2,21 @@ import { OpenAI } from "langchain/llms/openai";
 import { LLMChain } from "langchain/chains";
 import { InputValues } from "langchain/schema";
 import { PromptBaseService } from "./PromptBaseService";
+import { ChatOpenAI } from "langchain/chat_models";
+import { ChatPromptTemplate } from "langchain/prompts";
 
 export class ChainService {
   public static buildLLMChain<T extends InputValues>(
-    llm: OpenAI,
-    query: string,
+    llm: OpenAI | ChatOpenAI,
+    rawTemplate: string | ChatPromptTemplate,
     inputVars?: string[]
   ) {
     return new LLMChain({
       llm: llm,
-      prompt: PromptBaseService.buildPromptTemplate(query, inputVars),
+      prompt:
+        rawTemplate instanceof ChatPromptTemplate
+          ? rawTemplate
+          : PromptBaseService.buildPromptTemplate(rawTemplate, inputVars),
     });
   }
 }
